c8d594aefa5711a49f5ed9a8f0f61f45105a653c
Spider-Gwen <maquinterov0399@gmail.com>
2024-01-10 18:40:57 -0600

    ready for ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[p5r-cheatsheet]/"
+  base: "https://MCMike0399.github.io/p5r-cheatsheet"
 })
